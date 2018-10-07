7aa271be229f556c6b057dcdb5883e3182d8b961
helg1978 <a@ukr.net>
2018-10-07 22:54:25 +0300

    Fix for retina and 4k screens
    In case of  renderer.setPixelRatio(1) for window.devicePixelRatio>1 (4K screens) text gets very blury
    I have window.devicePixelRatio===2.5, and have bluring issues 
    P.S. Also, I was forced to add if (renderer&&renderer.domElement.width.... ) to avoid "Uncaught TypeError: Cannot read property 'domElement' of undefined" at start. 
    I am pretty sure that I call new THREE.TextSprite when renderer already exists.

--- src/getOptimalFontSize.js
+++ src/getOptimalFontSize.js
@@ -8,7 +8,7 @@ export default function(object, renderer, camera) {
 	if (renderer.domElement.width && renderer.domElement.height && object.material.map.textLines.length) {
 		let distance = object.getWorldPosition(objectWorldPosition).distanceTo(camera.getWorldPosition(cameraWorldPosition));
 		if (distance) {
-			let heightInPixels = object.getWorldScale(objectWorldScale).y * renderer.domElement.height / distance;
+			let heightInPixels = object.getWorldScale(objectWorldScale).y * renderer.domElement.height * window.devicePixelRatio/ distance;
 			if (heightInPixels) {
 				return Math.round(heightInPixels / object.material.map.imageHeight);
 			}
